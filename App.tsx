6333944a48220183f71b1b89f7a1ab61896b1eeb
pawida chumpurat <pawida.may@gmail.com>
2023-07-13 16:23:16 +0700

    add style to GlobalStyles and add component Button

--- App.tsx
+++ App.tsx
@@ -1,19 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , Switch} from 'react-native';
+import { ThemeContext } from './src/context/ThemeContext';
+import { useState } from 'react';
+import { myColors } from './src/styles/Colors';
 
 export default function App() {
+  const [theme, setTheme] = useState('light');
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <ThemeContext.Provider value={theme}>
+    <View style={theme === 'light' ? styles.container : [styles.container, {backgroundColor: '#000'}]}>
       <StatusBar style="auto" />
+      <Switch
+      value={theme === 'light'}
+      onValueChange={()=> setTheme(theme === 'light' ? 'dark' : 'light')}
+      />
     </View>
+    </ThemeContext.Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: myColors.light,
     alignItems: 'center',
     justifyContent: 'center',
   },
