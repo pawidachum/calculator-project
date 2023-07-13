87ea6ef6c8948d707d6069051d2e8c5cbac7f16b
pawida chumpurat <pawida.may@gmail.com>
2023-07-13 21:45:09 +0700

    add feature MyKeyboard

--- App.tsx
+++ App.tsx
@@ -1,22 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View , Switch} from 'react-native';
+import { StyleSheet, Text, View , Switch, SafeAreaView} from 'react-native';
 import { ThemeContext } from './src/context/ThemeContext';
 import { useState } from 'react';
 import { myColors } from './src/styles/Colors';
 import Button from './src/components/Button';
+import MyKeyboard from './src/components/MyKeyboard';
 
 export default function App() {
   const [theme, setTheme] = useState('light');
   return (
     <ThemeContext.Provider value={theme}>
-    <View style={theme === 'light' ? styles.container : [styles.container, {backgroundColor: '#000'}]}>
+    <SafeAreaView style={theme === 'light' ? styles.container : [styles.container, {backgroundColor: '#000'}]}>
       <StatusBar style="auto" />
       <Switch
       value={theme === 'light'}
       onValueChange={()=> setTheme(theme === 'light' ? 'dark' : 'light')}
       />
-      <Button isGray title='3' onPress={()=> {alert('hello')}}/>
-    </View>
+      <MyKeyboard/>
+    </SafeAreaView>
     </ThemeContext.Provider>
   );
 }
@@ -26,6 +27,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: myColors.light,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
 });
